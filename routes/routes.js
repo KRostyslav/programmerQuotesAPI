55120ca0c33fffceb1ad54a1ac33b842935f368d
krostyslav <rostyslav.kryvyy@gmail.com>
2020-11-15 13:40:37 +0200

    update json and add new endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,50 @@
 const appRouter = (app, fs) => {
-  // // variables
   const dataPath = './data/passionate.json';
 
-  // READ
-  app.get('/data', (req, res) => {
+
+  /**
+   * Get inforamation about book
+   */
+  app.get('/getInfo', (req, res) => {
     fs.readFile(dataPath, 'utf8', (err, data) => {
       if (err) {
         throw err;
       }
 
-      res.send(JSON.parse(data));
+      res.send(JSON.parse(data).bookInfo);
     });
   });
+
+  /**
+   * Get all quotes
+   */
+  app.get('/getAllQuotes', (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, data) => {
+      if (err) {
+        throw err;
+      }
+
+      res.send(JSON.parse(data).quotes);
+    });
+  });
+
+  /**
+   * Get random qoute
+   */
+  app.get('/getRandomQuote', (req, res) => {
+    fs.readFile(dataPath, 'utf8', (err, datafile) => {
+      if (err) {
+        throw err;
+      }
+      const data = JSON.parse(datafile);
+      const countQuote = data.quotes.length;
+      res.send(data.quotes[getRandomInt(countQuote)]);
+    });
+  });
+
+  function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
+  }
 };
 
 module.exports = appRouter;
